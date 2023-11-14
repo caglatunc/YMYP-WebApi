b43b6a6a541597a424d4766ccd1c5f817557ff09
caglatunc <caglatunc03@gmail.com>
2023-11-14 23:50:15 +0300

    Mentioned NTierArchitecture and starting ITDeskApp .

---
+++ FirstWebApi/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using GenericFileService.Files;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers;
+[ApiController]
+[Route("api/[controller]/[action]")]
+public sealed class HomeController : ControllerBase
+{
+    private readonly Test _test;
+
+    public HomeController(Test test)
+    {
+        _test = test;
+    }
+
+    [HttpGet] //metot tipleri
+    public IActionResult Get()
+    {
+        Test test = new();
+        test.Age = 1;
+        test.Name = "Cagla";
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    public string Get2()
+    {
+        return "Hello World";
+    }
+
+    [HttpPost]
+
+    public IActionResult SaveFile([FromForm] IFormFile file)
+    {
+        string fileName1 =  FileService.FileSaveToServer(file, "./wwwroot/files/");
+        string fileName2= FileService.FileSaveToServer(file, "C:/YMYP-Angular/file-management-app/src/assets/files/");
+        return Ok(file);
+    }
+
+    [HttpPost]
+
+    public IActionResult SaveFiles(List<IFormFile> files)
+    {
+        foreach(var item in files)
+        {
+            string fileName1 = FileService.FileSaveToServer(item, "./wwwroot/files/");
+            string fileName2 = FileService.FileSaveToServer(item, "C:/YMYP-Angular/file-management-app/src/assets/files/");
+        }
+        return Ok(files);
+    }
+
+    [HttpPost]
+    public IActionResult SaveWithFile([FromForm]Test2 test)
+    {
+        string fileName1 = FileService.FileSaveToServer(test.File, "./wwwroot/files/");
+        string fileName2 = FileService.FileSaveToServer(test.File, "C:/YMYP-Angular/file-management-app/src/assets/files/");
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult SaveWithFiles([FromForm] Test3 test)
+    {
+        foreach (var item in test.Files)
+        {
+            string fileName1 = FileService.FileSaveToServer(item, "./wwwrooot/files/");
+            string fileName2 = FileService.FileSaveToServer(item, "C:/YMYP-Angular/file-management-app/src/assets/files/");
+        }
+        return Ok();
+    }
+}
+
+
+public class Test
+{
+    public string Name { get; set; } // get set blokları bu değişkeni bir property olarak tanımlar.
+    public int Age { get; set; }
+}
+
+public class Test2
+{
+    public IFormFile File { get; set; }
+    public int Id { get; set; }
+}
+
+
+public class Test3
+{
+    public List<IFormFile> Files { get; set; }
+    public int Id { get; set; }
+}
---
+++ NTierArchitecture/NTierArchitecture.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace NTierArchitecture.WebApi.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ValuesController : Controller
+{
+    private static byte[] Hash;
+    private static byte[] Salt;
+    [HttpGet]
+   public IActionResult Encrypt(string password)
+    {
+        var hmac = new System.Security.Cryptography.HMACSHA512();
+        Salt = hmac.Key;
+        Hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));//hashleme
+
+        return Ok(new {Hash = Hash , Salt = Salt});    
+    }
+    [HttpGet]
+    public IActionResult Decode(string password)
+    {
+        var hmac = new System.Security.Cryptography.HMACSHA512(Salt);
+        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        for(int i=0; i < computeHash.Length; i++)
+        {
+            if (computeHash[i] != Hash[i])
+            {
+                return BadRequest();
+            }
+        }
+        return Ok();
+    }
+
+}
